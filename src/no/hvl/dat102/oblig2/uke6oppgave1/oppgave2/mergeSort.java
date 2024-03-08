c9ca7b7932cae617b86017eae5ab63d7add4d34e
669806 <669806@stud.hvl.no>
2024-03-08 13:23:12 +0100

    Create mergeSort.java

---
+++ src/no/hvl/dat102/oblig2/uke6oppgave1/oppgave2/mergeSort.java
@@ -0,0 +1,19 @@
+package no.hvl.dat102.oblig2.uke6oppgave1.oppgave2;
+
+import java.util.Arrays;
+
+public class mergeSort {
+	
+	private static <T extends Comparable<? super T>> mergeSort(Integer[] a) {
+		
+		if (a.length > 1) {
+			int mid = a.length / 2;
+			Integer[] venstre = Arrays.copyOfRange(a, 0, mid);
+			Integer[] høyre = Arrays.copyOfRange(a, mid, a.length);
+			mergeSort(venstre);
+			mergeSort(høyre);
+			merge(a, vesntre, høyre);
+		}
+	}
+
+}
