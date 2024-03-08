0431507556ef28fec1b0e3165350476a09277b01
669806 <669806@stud.hvl.no>
2024-03-08 22:04:04 +0100

    Update main.java

--- src/no/hvl/dat102/oblig2/uke6/oppgave2/main.java
+++ src/no/hvl/dat102/oblig2/uke6/oppgave2/main.java
@@ -2,12 +2,14 @@ package no.hvl.dat102.oblig2.uke6.oppgave2;
 
 import java.util.Arrays;
 
+import no.hvl.dat102.oblig2.uke6oppgave1.oppgave2.malTid;
+
 public class main {
 	
 	 public static void main(String[] args) {
 	        // Example usage and time measurement
 	        Integer[] a = {5, 2, 9, 1, 5, 6};
-	        long timeTaken = measureTime(a, "quickSort");
+	        long timeTaken = malTid.measureTime(a, "quickSort");
 	        System.out.println("Time taken: " + timeTaken + " nanoseconds");
 	        System.out.println(Arrays.toString(a));
 	    }
